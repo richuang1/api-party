35e90fa59ef888bc638698bb7d69c69fedfed99f
richuang1 <richuang@iu.edu>
2018-07-10 16:27:59 -0400

    Pokemon

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import { Route, NavLink, Switch } from 'react-router-dom'
 
 import './App.css'
-import Github from './Github'
-import Homework from './Homework'
+import Pokemon from './Pokemon'
 
 class App extends Component {
   render() {
@@ -15,18 +14,14 @@ class App extends Component {
         </header>
         <ul className="navLinks">
           <li>
-            <NavLink to="/github">GitHub API</NavLink>
-          </li>
-          <li>
-            <NavLink to="/homework">Homework</NavLink>
+            <NavLink to="/pokemon">Pok&eacute; API</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path="/github" component={Github} />
-          <Route path="/homework" component={Homework} />
+          <Route path="/pokemon" component={Pokemon} />
           <Route render={() => (
-              <p>To get started, click one of the links above.</p>
-          )} />
+            <p>To get started, click one of the links above.</p>
+          )}/>
         </Switch>
       </div>
     )
